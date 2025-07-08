5ab5f473136c3fa1b92a287e0428b131b8ac47aa
Vincent Xu <vincent.xu1@outlook.com>
2025-07-08 13:30:26 -0700

    Completed assignment 1.1.1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Assignment_1._1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Vincent Xu";
+            int age = 28;
+            string address = "3455 Senn St";
+            Console.WriteLine($"{name}, {age}, {address}");
+        }
+    }
+}
